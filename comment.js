d0f3adfa370d9e73baa4566b7bf685b584e84609
Nivetha <152204717+Nivi003@users.noreply.github.com>
2024-03-19 11:19:34 +0530

    All files are uploaded

---
+++ comment.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById('btns');
+    button.addEventListener('click', function(){
+        document.write("<h1>","Comments submitted successfully");
+    });
+});
---
+++ form.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById('btn');
+    button.addEventListener('click', function(){
+        document.write("<h1>","Registered successfully");
+    });
+});
